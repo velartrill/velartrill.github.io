cd0cec97faa72c084074c9153a09c2f2b454d2e2
velartrill <velartrill@swordfish.fake>
2014-07-24 01:53:36 -0700

    add MV analytics, create real index

--- modernvillainy/makesite.rb
+++ modernvillainy/makesite.rb
@@ -40,6 +40,16 @@ def makepage(page, depth)
 		<meta charset="utf-8">
 		<title>Modern Villainy</title>
 		<link rel="stylesheet" href="'+makeurl('modernvillainy.css',depth)+'">
+		<link href="http://fonts.googleapis.com/css?amp;family=Open+Sans:100,300italic,400,300,700&amp;subset=latin,latin-ext" rel="stylesheet" type="text/css" />
+		<script>
+		  (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,"script","//www.google-analytics.com/analytics.js","ga");
+
+		  ga("create", "UA-51096979-1", "velartrill.github.io");
+		  ga("send", "pageview");
+		</script>
 	</head>
 <body>
 <div id="header"></div>
